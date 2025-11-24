7e784a153e2b741f14c5de91ac89ca33140e7050
dhruv <dhruvgupta742@gmail.com>
2025-11-25 00:40:01 +0530

    Concurrency opt

--- backend/controller/ws/subsciber.go
+++ backend/controller/ws/subsciber.go
@@ -1,30 +1,65 @@
 package ws
 
 import (
-    "context"
+	"sync"
+	"context"
 	"encoding/json"
 
+	"github.com/MR-DHRUV/snake_and_ladders/constants"
 	"github.com/MR-DHRUV/snake_and_ladders/db"
+	gameHandler "github.com/MR-DHRUV/snake_and_ladders/handler/game"
 	"github.com/MR-DHRUV/snake_and_ladders/model"
 	"github.com/MR-DHRUV/snake_and_ladders/transport"
-	"github.com/MR-DHRUV/snake_and_ladders/constants"
-	gameHandler "github.com/MR-DHRUV/snake_and_ladders/handler/game"
 )
 
+type gameChannel struct {
+	EventChan chan *model.GameEvent
+}
+
+var channelMap sync.Map // map[gameId]*gameChannel
+
+func getOrCreateWorker(gameId string) *gameChannel {
+	if w, ok := channelMap.Load(gameId); ok {
+		return w.(*gameChannel)
+	}
+
+	// create new worker
+	worker := &gameChannel{
+		EventChan: make(chan *model.GameEvent, 100),
+	}
+
+	channelMap.Store(gameId, worker)
+
+	// launch worker goroutine
+	go startWorker(gameId, worker)
+
+	return worker
+}
+
+func startWorker(gameId string, worker *gameChannel) {
+	ctx := &gameHandler.Context{
+		ConnectionManager: transport.GetConnectionManager(),
+		GameId:            gameId,
+		UserId:            "redis_worker",
+		UserName:          "redis_worker",
+	}
+
+	for evt := range worker.EventChan {
+		gameHandler.HandleBroadcast(ctx, evt)
+	}
+}
+
 func StartGameEventSubscriber() {
-    pubsub := db.RedisClient.PSubscribe(context.Background(), constants.RedisChannelPrefix+"*"+constants.RedisChannelSuffix)
+	pubsub := db.RedisClient.PSubscribe(context.Background(), constants.RedisChannelPrefix+"*"+constants.RedisChannelSuffix)
 
 	for msg := range pubsub.Channel() {
 		gameId := msg.Channel[len(constants.RedisChannelPrefix) : len(msg.Channel)-len(constants.RedisChannelSuffix)]
+
 		evt := model.GameEvent{}
 		json.Unmarshal([]byte(msg.Payload), &evt)
 
-		// broadcast to clients of the game
-		go gameHandler.HandleBroadcast(&gameHandler.Context{
-			ConnectionManager: transport.GetConnectionManager(),
-			GameId: gameId,
-			UserId: "redis_subscriber",
-			UserName: "redis_subscriber",
-		}, &evt)
+		// Push into worker queue for sequential processing
+		worker := getOrCreateWorker(gameId)
+		worker.EventChan <- &evt
 	}
 }
